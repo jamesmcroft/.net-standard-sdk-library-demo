4b018ecd7df32841fda6d77d5a102fe21bec8976
James Croft <jamesmcroft@users.noreply.github.com>
2019-07-23 13:03:08 +0100

    Completed demo (#1)
    * Re-added basic .NET Standard SDK style project with NuSpec
    * Re-added project with multi-targetting and platform specific conditionals

---
+++ src/multitarget/MySDK/MySDK.Conditional/Devices/Device.iOS.cs
@@ -0,0 +1,31 @@
+#if __IOS__
+namespace MySDK.Devices
+{
+    using UIKit;
+
+    /// <summary>
+    /// Defines an implementation of <see cref="IDevice"/> for iOS.
+    /// </summary>
+    public class Device : IDevice
+    {
+        /// <summary>
+        /// Gets the name of the device.
+        /// </summary>
+        public string Name => "iPad";
+
+        /// <summary>
+        /// Get the form factor of the device.
+        /// </summary>
+        public FormFactor FormFactor => FormFactor.Tablet;
+
+        /// <summary>
+        /// Gets the version number of the current OS.
+        /// </summary>
+        /// <returns>The version number as a <see cref="string"/>.</returns>
+        public string GetOSVersion()
+        {
+            return UIDevice.CurrentDevice.SystemVersion;
+        }
+    }
+}
+#endif
